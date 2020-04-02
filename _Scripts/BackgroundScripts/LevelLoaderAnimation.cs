33d8fd907f202fb569287d2e51042f5b142ccce6
gsimon321 <60612435+gsimon321@users.noreply.github.com>
2020-04-02 02:21:20 -0400

    changes to background scripts
    Deleted Scripts: LevelLoader (this was inside of the BackGround Folder)

--- _Scripts/BackgroundScripts/LevelLoaderAnimation.cs
+++ _Scripts/BackgroundScripts/LevelLoaderAnimation.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoaderAnimation : MonoBehaviour
 {
+    //These are all of the variables that need to be initailized
+    //this is the animator that will take care of the transition
     public Animator transition;
+    //this will be the sceneNumber that it switches to
     public int sceneNumber;
+    //this is the time taken for transition
     public float transitionTime = 1f;
 
     // Start is called before the first frame update
@@ -14,21 +18,25 @@ public class LevelLoaderAnimation : MonoBehaviour
     {
         
     }
+    //this will ocur when the trigger is hit
     public void OnTriggerEnter2D(Collider2D theCollider)
     {
-
+        //will load the next scene
         LoadNextLevel();
 
 
     }
-
+    //this is incharge of loading the next scene
     public void LoadNextLevel()
     {
+        //This starts a coroutine that loads the next scene based on the scene number
         StartCoroutine(LoadLevel(sceneNumber));
     }
 
+    //this is incharge of loading the next level
     IEnumerator LoadLevel(int levelIndex)
     {
+        //sets the transition and starts the new level using a corountine so that it doesnt interupt other code
         transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(transitionTime);
--- _Scripts/BackgroundScripts/SceneSwitcher.cs
+++ _Scripts/BackgroundScripts/SceneSwitcher.cs
@@ -6,13 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-
+    //variable that correlates to which scene to switch to
     public int myScene;
 
     public void OnTriggerEnter2D(Collider2D theCollider)
     {
+        //only switches to scene if the object that triggers the collision event is tagged as the player
         if (theCollider.CompareTag("player"))
         {
+            //this will load the new scene
             SceneManager.LoadScene(myScene, LoadSceneMode.Single);
         }
        
